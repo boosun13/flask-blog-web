2260c413fae5c5be7cf438cb39a5d794480faf70
boosun13 <double0two22@gmail.com>
2023-10-15 14:56:58 +0900

    chore: chakra-ui を導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { NextPage } from 'next'
 import RootLayout from '../app/layout'
+import { ChakraProvider, Box, Button } from '@chakra-ui/react'
 
 const Page: NextPage = () => {
   return (
     <RootLayout>
-      <h1>Hello, Home page!</h1>
+      <ChakraProvider>
+        <Box mb={3}>
+          <h1>Hello, Home page!</h1>
+        </Box>
+
+        <Box mb={3}>
+          <Button>OK</Button>
+        </Box>
+      </ChakraProvider>
     </RootLayout>
   )
 }
